e9e839892511d868e8fd4fa0073286207baae8d5
Tomás Rama <tomas.rama@correo.ucu.edu.uy>
2021-09-27 19:47:07 -0300

    Cambios

--- src/Library/Greeter.cs
+++ src/Library/Greeter.cs
@@ -4,25 +4,11 @@ namespace Library
 {
     public class Greeter
     {
-        private Greeter()
-        {
+        //private Greeter()
+        //{
             // Intentionally left blank
-        }
+        //}
 
-        private static Greeter instance;
-
-        public static Greeter Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = new Greeter();
-                }
-
-                return instance;
-            }
-        }
 
         public void SayHiToTheWorld()
         {
---
+++ src/Library/Singleton.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Library
+{
+    public class Singleton<T> where T : new()
+    {
+        private static T instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new T();
+                }
+
+                return instance;
+            }
+        }
+    }
+
+}
